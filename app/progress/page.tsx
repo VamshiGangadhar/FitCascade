bfa1653aa2372fec33ac15fee77707b5145f588a
VamshiGangadhar <vamshi.gangadhar365@gmail.com>
2024-12-30 15:26:37 +0530

    fix eslint

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
